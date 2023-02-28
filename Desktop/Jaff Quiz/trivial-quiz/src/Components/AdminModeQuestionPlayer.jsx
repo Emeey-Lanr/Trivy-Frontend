db08256282cce8553c713809fcf6d478ca724c89
Emeey-Lanr <emeeylanr04@gmail.com>
2023-02-28 08:07:26 +0100

    worked on the accepting the question and sending it back when the timer is zero on the admin end

---
+++ Desktop/Jaff Quiz/trivial-quiz/src/Components/AdminModeQuestionPlayer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminModeQuestionPlayer = () => {
+  return (
+    <div>AdminModeQuestionPlayer</div>
+  )
+}
+
+export default AdminModeQuestionPlayer
--- Desktop/Jaff Quiz/trivial-quiz/src/Components/Game.jsx
+++ Desktop/Jaff Quiz/trivial-quiz/src/Components/Game.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect, useContext, createContext } from "react";
-import { FaInfo } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 import { appContext } from "../App";
 import AdminPlayers from "./AdminPlayers";
 import AdminQuestion from "./AdminQuestion";
@@ -12,11 +12,15 @@ import { Oval } from "react-loader-spinner";
 import StartGameLoading from "./StartGameLoading";
 import TopActAnimation from "./TopActAnimation";
 import OverallResult from "./OverallResult";
+import AdminModeQuestionPlayer from "./AdminModeQuestionPlayer";
+import UserModeQuestionPlayer from "./UserModeQuestionPlayer";
+import IndividualGameLogin from "./IndividualGameLogin";
 export const gameContext = createContext();
 const Game = () => {
   const { socket, gameEndPoint, userName } = useContext(appContext);
   const [admin, setAdmin] = useState(false);
   const [socketId, setSocketId] = useState("");
+  let date = new Date()
   const [clicked, setClicked] = useState(1);
   // for the user to know which button is being clicked
   const [startBtnStyle, setStartBtnStyle] = useState({
@@ -27,6 +31,7 @@ const Game = () => {
   const [startOrPlayerJoining, setStartOrPlayerJoining] = useState(1);
   // admin stage show which stage admin is
   const [adminStage, setAdminStage] = useState("AdminPage01");
+  const [quizMode, setQuizMode] = useState(0)
   const [roomId, setRoomId] = useState("");
   // adminQuestion
   const [adminQuestion, setAdminQuestion] = useState({
@@ -73,7 +78,11 @@ const Game = () => {
 
   //  time 
   const [minutes, setMinutes] = useState(-1)
-  const [second, setSecond] =useState(-1)
+  const [second, setSecond] = useState(-1)
+  const [timerStatus, setTimerStatus] = useState(false)
+
+  // self
+  const [selTime, setSelfTime] = useState(0)
   const getSocketIdFunction = () => { };
   // completed message
   const [completedMessage, setCompletedMessage] = useState("")
@@ -92,12 +101,17 @@ const Game = () => {
           if (result.data.adminStatus) {
             console.log(result.data)
             setAdmin(result.data.adminStatus);
+            // setAdmin(false)
             setRoomId(result.data.quizIdNumberPlayedId);
             // setAdminQuestion(result.data.questionToBeAnswered);
             setAdminQuestionTitle(result.data.questionToBeAnswered);
+            setQuizMode(result.data.mode)
             socket.current.emit("createRegistrationBox", {
               quizId: result.data.quizIdNumberPlayedId,
               adminPage: adminStage,
+              mode:result.data.mode,
+             
+              date:new Date
             });
           } else {
             setIndividualName(result.data.userDetails.playerName);
@@ -246,7 +260,7 @@ const ifItExistAndProbablyReload = () => {
       socket.current.on("changeToVictoryPage", (data) => {
         setWait(false)
         setAdminStage(data.adminStage);
-        timeFunction(false)
+  
         setPlayerScore(data.playerRanking)
         setPlayerScoreGameIndex(data.currentIndex);
         setCurrentSubject(data.currentSubject);
@@ -325,13 +339,127 @@ const ifItExistAndProbablyReload = () => {
   
   var timer 
   const [place, setplace] = useState(0)
-  const timeFunction = (   ) => {
-      if (wait) {
+
+  //Stage 3
+ // self more like a cbt question
+  // user
+  const [selfQuizQuestion, setSelfQuizQuestion] = useState([]);
+  const [selfCurrentQuestion, setSelfCurrentQuestion] = useState({});
+  const [subjectScoreBox, setSubjectScoreBox] = useState([])
+  const [quizNumberAttempted, setQuizNumberAttempted] = useState([])
+  const [knowPickedAnswer, setPickedAnwser] = useState([])
+  
+ 
+  const startStage3QuestionFunction = () => {
+    if (socket.current) {
+      socket.current.on("stage3Question", (data) => {
+        console.log(data)
+        setAdminStage(data.adminPage);
+        setStartLoading(true)
+        setCurrentSubjectName(data.subjectName);
+        setSelfQuizQuestion(data.question);
+        setSelfCurrentQuestion(data.question[currentQuestionIndex]);
+        setQuizNumberAttempted(data.scoreBox);
+        setQuizNumberAttempted(data.scoreBox);
+        setPickedAnwser(data.scoreBox);
+        setCurrentSecond(data.time);
+        setAssignedMark(data.subjectMark);
+        console.log(quizMode)
+        setTimeout(() => {
+          setQuizMode(data.mode)
+          setWait(true)
+          setStartLoading(false)
+        }, 1000)
         
-      }
-    
+
+      });
+    }
   }
+
+  // what shows on the adminpage is the time while the participants answer the question
+ 
+  const adminPageWhileQuizFunction = () => {
+    if (socket.current) {
+      if (admin) {
+        socket.current.on("stage3AdminQuestion", (data) => {
+          if (data.adminId === roomId) {
+            setStartLoading(true)
+            setTimeout(() => {
+               setAdminStage(data.adminPage);
+               setCurrentSecond(data.time);
+               setWait(true);
+               setStartLoading(false)
+            },1000)
+           
+
+          }
+        });
+      }
+    }
+  }
+const [sendResult, setSendResult] = useState(0)
+const [userNamee, setUserName] = useState("")
+
+  const submitAnswerFunction = () => {
+    let totalScore = 0;
+    if (socket.current) {
+  
+      socket.current.on("collectPlayersAnswers", (data) => {
+        const calculate =()=>{
+        return new Promise((resolve, reject) => {
+             console.log(individualName, roomId);
+             setUserName(individualName);
+             
+           console.log(sendResult)
+           setStartLoading(true)
+              const error = false;
+                if (!error) {
+                  resolve();
+                } else {
+                  reject();
+                }
+            
+        })
+        }
+     
+  const sendMessage = ()=>{
+    return new Promise((resolve, reject) => {
+      socket.current.emit("verifyToSubmitAnswer", {
+        username: individualName,
+        roomId: data.roomId,
+        currentSubject: currentSubjectName,
+        totalScore:subjectScoreBox,
+        status: true,
+      });
+
+      const error = false
+      if(!error){
+        resolve()
+      }else{
+        reject()
+      }
+    })
+  }
+  const sendAll = async()=>{
+await calculate()
+await sendMessage()
+  }
+  sendAll()
+  
+})
+      
+      // we will collect the current subject
+      // the name, the roomId
+      
+    }
+  }
+  
   useEffect(() => {
+
+    // stage1 the admin has the control to read the question, next 
+
+
+    // stage2, this is the automatic one that runs based on the time
     ifItExistAndProbablyReload();
     checkIfAPlayerJoined();
     ifYouJoined();
@@ -342,11 +470,16 @@ const ifItExistAndProbablyReload = () => {
     ifGamePlayed()
     adminNextQuestion()
     changeToSemiVictoryStage()
-    timeFunction()
     changeNextSubjectFunction()
     changeToVictoryPageFunction()
     loadSpinnerFunction()
     whenResultIsSavedFunction()
+
+    // stage 3
+    // self more like a cbt question
+    startStage3QuestionFunction()
+    adminPageWhileQuizFunction()
+    submitAnswerFunction()
 
     if (wait) {
       timer = setInterval(() => {
@@ -355,12 +488,27 @@ const ifItExistAndProbablyReload = () => {
         if (currentSecond === 0) {
           if (admin) {
             if (admin) {
-              socket.current.emit("changeQuestion", {
-                roomId: roomId,
-                currentQuestion: currentQuestion[currentQuestionIndex],
-              });
-              setWait(false)
+              if (quizMode === 1) {
+                
+              } else if (quizMode === 2) { 
+                socket.current.emit("changeQuestion", {
+                  roomId: roomId,
+                  currentQuestion: currentQuestion[currentQuestionIndex],
+                });
+                setWait(false);
+
+              } else if (quizMode === 3) {
+                socket.current.emit("selfSubmitQuestion", {
+                  roomId: roomId,
+                
+                })
+                setStartLoading(true)
+                setWait(false)
+              }
+              
             }
+          }else{
+            setWait(false)
           }
         }  
          
@@ -391,9 +539,11 @@ const ifItExistAndProbablyReload = () => {
   const [nextBtn, setNextBtn] = useState(false)
 
   const startQuizBtn = (quizname, id) => {
-      console.log(adminQuestionTitle[id].subjectMark);
-
-    setAdminStage("AdminPage02");
+    console.log(adminQuestionTitle[id].subjectMark);
+    if (quizMode === 1) {
+      // here there will be button for each question where the admin can read assign time and next
+    } else if (quizMode === 2) {
+       setAdminStage("AdminPage02");
     setStartLoading(true);
     socket.current.emit("switchToLoad", {
       adminPage: "AdminPage02",
@@ -420,6 +570,17 @@ const ifItExistAndProbablyReload = () => {
 
       // setWait(true);
     }, 2000);
+    } else if (quizMode === 3) {
+      // here the admin will give the student a time to answer all the whole questions
+      setCurrentSubject(adminQuestionTitle[id].quizName);
+      setPlayerQuestion(adminQuestionTitle[id].questions);
+   
+      // console.log(adminQuestionTitle[id])
+      // console.log();
+      setTimerStatus(true)
+    }
+
+   
   };
 
 
@@ -450,6 +611,77 @@ const ifItExistAndProbablyReload = () => {
     // alert(20)
     socket.current.emit("completedQuiz", {gameId:roomId})
   }
+
+
+
+  // stage 3
+  const startSelfModeBtn = () => {
+    if (selTime > 0) {
+      socket.current.emit("startSelfMode", {
+        question: playerQuestion,
+        currentSubjectName: currentSubject,
+        roomId:roomId,
+        allSubjects:adminQuestionTitle,
+        time:selTime
+      });
+      setAdminStage("AdminPage02");
+      setStartLoading(true)
+      setTimerStatus(false)
+      
+   
+    } else {
+      alert("pls input a specific time")
+    }
+    
+  }
+
+  // next and prev btn
+  const selfPrevBtn = () => {
+    if (currentQuestionIndex === 0) {
+      setCurrentQuestionIndex(selfQuizQuestion.length - 1)
+    } else {
+      setCurrentQuestionIndex(currentQuestionIndex - 1)
+    }
+    
+    setSelfCurrentQuestion(selfQuizQuestion[currentQuestionIndex]);
+    
+  }
+  const selfNextBtn = () => {
+
+    setCurrentQuestionIndex(currentQuestionIndex + 1)
+    // setSelfCurrentQuestion(selfQuizQuestion(2))
+    if (currentQuestionIndex === selfQuizQuestion.length - 1) {
+      setCurrentQuestionIndex(0)
+    }
+    setSelfCurrentQuestion(selfQuizQuestion[currentQuestionIndex])
+    
+  }
+  useEffect(() => {
+    setSelfCurrentQuestion(selfQuizQuestion[currentQuestionIndex]); 
+  },[currentQuestionIndex])
+  const selfQuestionIndicatorBtn = (id) => {
+    setCurrentQuestionIndex(id)
+
+  }
+
+
+  const selfPickAnswerBtn = (status, id) => {
+    
+    const changeStyle = [...knowPickedAnswer]
+    changeStyle[currentQuestionIndex] = id + 1
+    setPickedAnwser(changeStyle)
+    if (status) {
+    const item = [...subjectScoreBox]
+   item[currentQuestionIndex]  = assignedMark
+      setSubjectScoreBox(item)
+    }
+    
+    // It shows youv've attempted a question and the number of questions attempted
+    const attemptedQuestion = [...quizNumberAttempted]
+    attemptedQuestion[currentQuestionIndex] = 1
+    setQuizNumberAttempted(attemptedQuestion)
+
+  }
   return (
     <gameContext.Provider
       value={{
@@ -470,9 +702,20 @@ const ifItExistAndProbablyReload = () => {
         currentMinutes,
         currentSecond,
         currentSubject,
+        currentSubjectName,
         nextSubjectOrOverallResult,
         quizCompleted,
         completedMessage,
+
+        // stage 3 self
+        currentQuestionIndex,
+        selfCurrentQuestion,
+        quizNumberAttempted,
+        knowPickedAnswer,
+        selfNextBtn,
+        selfPrevBtn,
+        selfQuestionIndicatorBtn,
+        selfPickAnswerBtn,
       }}
     >
       <div>
@@ -525,9 +768,19 @@ const ifItExistAndProbablyReload = () => {
                     <StartGameLoading />
                   </div>
                 ) : (
-                  <div>
-                    <AdminQuestion />
-                  </div>
+                  <>
+                    {quizMode === 1 && <div></div>}
+                    {quizMode === 2 && (
+                      <div>
+                        <AdminQuestion />
+                      </div>
+                    )}
+                    {quizMode === 3 && (
+                      <div className="w-10p h-10p fixed top-0 bg-green-like-100 flex justify-center items-center">
+                        <div className="w-dw h-dw"><p className="text-white  text-5xl text-serif texty">{currentSecond}</p></div>
+                      </div>
+                    )}
+                  </>
                 )}
               </div>
             )}
@@ -576,9 +829,29 @@ const ifItExistAndProbablyReload = () => {
                     <StartGameLoading />
                   </div>
                 ) : (
-                  <div>
-                    <PlayerQuestion />
-                  </div>
+                  // <div>
+                  //
+                  // </div>
+                  <>
+                    {/* this one has button to next the question */}
+                    {quizMode === 1 && (
+                      <div>
+                        <AdminModeQuestionPlayer />
+                      </div>
+                    )}
+                    {quizMode === 2 && (
+                      // this one is automatic
+                      <div>
+                        <PlayerQuestion />
+                      </div>
+                    )}
+                    {quizMode === 3 && (
+                      <div>
+                        {/* this place the user is given all the question to answer */}
+                        <UserModeQuestionPlayer />
+                      </div>
+                    )}
+                  </>
                 )}
               </div>
             )}
@@ -607,6 +880,32 @@ const ifItExistAndProbablyReload = () => {
                 <StartGameLoading />
               </div>
             )}
+          </div>
+        )}
+        {timerStatus && (
+          <div className="w-10p h-10p fixed top-0 flex justify-center items-center bg-modalback">
+            <div className="bg-white rounded-sideicon w-createModalSize">
+              <div className="w-10p flex justify-end px-1 py-1">
+                <button onClick={() => setTimerStatus(false)}>
+                  <FaTimes className="text-green-like-100" />
+                </button>
+              </div>
+              <div className="w-9p mx-auto flex justify-center my-4">
+                <input
+                  onChange={(e) => setSelfTime(e.target.value)}
+                  type="number"
+                  className="border border-green-like-100 h-7 outline-green-like-100 font-mono font-bold  text-center text-lg text-green-like-100"
+                />
+              </div>
+              <div className="w-9p mx-auto">
+                <button
+                  onClick={() => startSelfModeBtn()}
+                  className="bg-green-like-100 py-4 w-10p text-white font-bold"
+                >
+                  Start
+                </button>
+              </div>
+            </div>
           </div>
         )}
       </div>
